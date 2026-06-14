9238c8ea7ae96ba9e3ae49f81f851c0dfa858e60
Yogeth <yogethcr@gmail.com>
2026-06-14 10:23:10 +0530

    update Contact.jsx and contact.css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,11 +5,25 @@ export default function Contact() {
     <div id="contactPage">
       <section id='section1'>
         <h1 style={{fontSize:'26pt',letterSpacing:'1px'}}>Initialize <span style={{color:'aqua'}}>Connection</span></h1>
-      <p style={{fontSize:'15pt'}}>{ContactData.para}</p>
+      <p>{ContactData.para}</p>
       </section>
-      <section id='section2'>
-          
-      </section>
+      <form id='section2'>
+          <label htmlFor="name">NAME
+            <input type="text" />
+          </label>
+          <label htmlFor="email">EMAIL_ADDRESS
+            <input type="text" />
+          </label>
+          <label htmlFor="body"> TRANSMISSION_BODY
+            <textarea id="txtarea" >
+
+            </textarea>
+          </label>
+           <button type='submit' id='formbtn'>
+                 EXECUTE_SEND 
+           </button>
+      </form>
+
     </div>
   )
 }
