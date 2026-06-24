e3b95eb7f494fb4b3fe83c6b5f6982c1b3eaf149
yogeth <yogethcr@gmail.com>
2026-06-24 13:04:30 +0530

    delete 1 file and update 1 file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
             </p>
           </Link>
           <a
-            href="/Yogeth_CR_Resume.pdf"
+            href="/Yogeth_Resume.docx"
             download="Yogeth_Resume"
             className=" border p-3 w-fit rounded-lg font-medium 
             text-lg xl:rounded-none"
